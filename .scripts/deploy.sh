1b0da8251e9df6f803b1bf735f05689249df04fc
Bernardo Martínez Garrido <programming@wandrell.com>
2015-07-08 09:50:54 +0200

    Correctected deployment script.

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -9,12 +9,12 @@
 # - DEPLOY: boolean, control flag for deployment, should be true to deploy
 # - CMS_BRANCH: string, the CMS branch from which the code has been taken
 
-if [ "$PULL_REQUEST" == "false" ] && [ "$DEPLOY" == "true" ] && [ "$CMS_BRANCH" == "master"]; then
+if [ "$PULL_REQUEST" == "false" ] && [ "$DEPLOY" == "true" ] && [ "$CMS_BRANCH" == "master" ]; then
 
     echo "Deploying template"
 
-    find . -type f -exec curl --user $REPO_USER:$REPO_PASSWORD --ftp-create-dirs -T {} $REPO_URL{} \;
-   
+    find . -type f -exec curl --user "$REPO_USER:$REPO_PASSWORD" --ftp-create-dirs -T {} "$REPO_URL{}" \;
+
 else
 
     echo "Template won't be deployed"
