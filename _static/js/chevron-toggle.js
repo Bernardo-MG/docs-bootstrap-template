bccbd521891ce271e3bfdd0202c6589cbbe20536
Bernardo Martínez Garrido <programming@wandrell.com>
2015-07-12 22:26:42 +0200

    Cleaned up code.

--- _static/js/chevron-toggle.js
+++ _static/js/chevron-toggle.js
@@ -1,27 +1,44 @@
 /**
- * Initializes the dropdown menus, so the chevrons get toggled.
+ * Functions for toggling chevrons on menus, so they indicate if the menu is deployed or not.
+ *
+ * This is purely for visual purposes. While chevrons help to indicate a menu option can be opened, animating them
+ * just looks good.
+ *
+ * Currently only dropdown menus are supported, and they should be marked with the .chevron_toggleable class. Also
+ * they should use the Font Awesome chevrons, and Bootstrap CSS classes.
+ *
+ * The way the chevrons work is simple: the one pointing up indicates the menu is closed, while the one pointing down
+ * indicates it is open. All menus should have the closed chevron by default on the html file.
+ *
+ * Loading this file will automatically initialize the scripts. This way the menus work just by adding it.
+ */
+
+/**
+ * Initializes the chevron toggling.
+ *
+ * This will prepare only the dropdown menus.
  */
 $(document).ready(function () {
 
-    $('.dropdown').on('show.bs.dropdown', toggleChevronDown);
+    $('.dropdown').on('show.bs.dropdown', toggleDropdownChevronOpen);
 
-    $('.dropdown').on('hide.bs.dropdown', toggleChevronRight);
+    $('.dropdown').on('hide.bs.dropdown', toggleDropdownChevronClosed);
 
 });
 
 /**
- * Toggles the chevron on the active dropdown menu, so it points down.
+ * Toggles the chevron on the active dropdown menu, so it points down, indicating it is open.
  */
-function toggleChevronDown() {
+function toggleDropdownChevronOpen() {
     $(this).find('.chevron_toggleable')
         .removeClass("fa fa-chevron-up")
         .addClass("fa fa-chevron-down");
 }
 
 /**
- * Toggles the chevron on the active dropdown menu, so it points right.
+ * Toggles the chevron on the active dropdown menu, so it points up, indicating it is closed.
  */
-function toggleChevronRight() {
+function toggleDropdownChevronClosed() {
     $(this).find('.chevron_toggleable')
         .removeClass("fa fa-chevron-down")
         .addClass("fa fa-chevron-up");
