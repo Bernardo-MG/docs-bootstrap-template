29688de8e722cb99271a738d6e5cf6c90b30d1cc
Bernardo Martínez Garrido <programming@wandrell.com>
2015-07-14 11:23:16 +0200

    Fixed delete script.

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -16,7 +16,7 @@ if [ "$PULL_REQUEST" == "false" ] && [ "$DEPLOY" == "true" ] && [ "$CMS_BRANCH" 
     for i in `curl -s -l ftp://"$REPO_USER":"$REPO_PASSWORD"@$ftp_ip/$ftp_path/ | grep *.*`; do
     {
            echo "deleting $i";
-           curl REPO_URL/${i} --user "${REPO_USER}:${REPO_PASSWORD}" -O --quote "DELE ${REPO_URL}/${i}"
+           curl $REPO_URL/${i} --user "${REPO_USER}:${REPO_PASSWORD}" -O --quote "DELE ${REPO_URL}/${i}"
     };
     done;
 
